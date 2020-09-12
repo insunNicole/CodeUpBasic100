7b6cb26bf98740ae66818b73512a1b282d4c9af1
insunNicole <florachoi1124@naver.com>
2020-09-12 09:34:43 +0900

    Basic1012 풀이

---
+++ src/Basic1012.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Basic1012 {
+    public static void main (String[] args) {
+        float num = new Scanner(System.in).nextFloat();
+        System.out.printf("%f", num);
+    }
+}
