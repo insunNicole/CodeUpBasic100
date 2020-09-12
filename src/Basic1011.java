de6c6bd1a8010f447828789c1cda6072a1d0fed7
insunNicole <florachoi1124@naver.com>
2020-09-12 09:33:46 +0900

    Basic1011 풀이

---
+++ src/Basic1011.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Basic1011 {
+    public static void main (String[] args) {
+        char word = new Scanner(System.in).next().charAt(0);
+        System.out.println(word);
+    }
+}
