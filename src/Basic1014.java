1332f85e7624f743d0bb2fc1a8e66b436eff9e9b
insunNicole <florachoi1124@naver.com>
2020-09-12 09:36:45 +0900

    Basic1014 풀이

---
+++ src/Basic1014.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Basic1014 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char word1 = sc.next().charAt(0);
+        char word2 = sc.next().charAt(0);
+        System.out.println(word2);
+        System.out.println(word1);
+    }
+}
