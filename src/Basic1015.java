07ef2327084a12d97dbcc99f6b137e3094f9e8e5
insunNicole <florachoi1124@naver.com>
2020-09-12 09:37:26 +0900

    Basic1015 풀이

---
+++ src/Basic1015.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Basic1015 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float num = sc.nextFloat();
+        System.out.printf("%.2f", num);
+    }
+}
