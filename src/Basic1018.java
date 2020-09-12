908a4a92553c1ea5ce6cf93f8587f41ba6f605eb
insunNicole <florachoi1124@naver.com>
2020-09-12 09:40:24 +0900

    Basic1018 풀이

---
+++ src/Basic1018.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Basic1018 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String time = sc.next();
+        System.out.println(time);
+    }
+}
