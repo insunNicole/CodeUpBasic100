899eb606f45c0eeb5b9abd3a98f41e501515ead3
insunNicole <florachoi1124@naver.com>
2020-09-13 20:41:28 +0900

    Basic1031 풀이

---
+++ src/Basic1031.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Basic1031 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%o", sc.nextInt());
+    }
+}
