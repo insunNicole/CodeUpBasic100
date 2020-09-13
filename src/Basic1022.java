488c92739e361787ebd201356493484edf1d1cc4
insunNicole <florachoi1124@naver.com>
2020-09-13 16:05:46 +0900

    Basic1022 풀이

---
+++ src/Basic1022.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Basic1022 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine());
+    }
+}
