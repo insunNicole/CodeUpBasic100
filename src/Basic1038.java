62c7caf49ab4042d75298f040eb8833ca85da091
insunNicole <florachoi1124@naver.com>
2020-09-13 21:28:05 +0900

    Basic1038 풀이

---
+++ src/Basic1038.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Basic1038 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLong() + sc.nextLong());
+    }
+}
